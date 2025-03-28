aeb91205033637907c580bf9281605fd1f74dcce
Raul Sabadin <raulsaba96@gmail.com>
2025-03-28 18:10:30 -0300

    recovery page layout

--- lib/features/auth/ui/recovery_secret/recovery_secret_page.dart
+++ lib/features/auth/ui/recovery_secret/recovery_secret_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dev_test/features/auth/ui/recovery_secret/resources/recovery_secret_page_assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class RecoverySecretPage extends StatelessWidget {
   const RecoverySecretPage({super.key});
@@ -7,9 +10,76 @@ class RecoverySecretPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.keyboard_arrow_left),
-          onPressed: () => Navigator.of(context).pop(),
+        leading: IconButton(icon: const Icon(Icons.keyboard_arrow_left), onPressed: () => Navigator.of(context).pop()),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 4),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Verificação",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              "Insira o código que foi enviado:",
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 64),
+            Row(
+              children: List.generate(6, (index) {
+                return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      maxLength: 1,
+                      decoration: const InputDecoration(
+                        counterText: "",
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+            const SizedBox(height: 32),
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: () {},
+                child: const Text("Confirmar"),
+              ),
+            ),
+            const SizedBox(height: 32),
+            Center(
+              child: TextButton(
+                onPressed: () => context.go("/home"),
+                child: SizedBox(
+                  width: 155,
+                  child: Center(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          RecoverySecretPageAssets.messageQuestionIcon,
+                          width: 20,
+                          height: 20,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          "Não recebi o código",
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/features/auth/ui/recovery_secret/resources/recovery_secret_page_assets.dart
@@ -0,0 +1,4 @@
+class RecoverySecretPageAssets {
+  static const String _basePath = 'assets/recovery_secret_page';
+  static const String messageQuestionIcon = '$_basePath/message-question.svg';
+}
