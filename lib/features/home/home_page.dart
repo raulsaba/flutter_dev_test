6903fb67e1f79c18adc556d011f6f1bf9ae8a255
Raul Sabadin <raulsaba96@gmail.com>
2025-03-28 18:18:53 -0300

    home exit button

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: IconButton(
+          onPressed: () {
+            context.go("/");
+          },
+          icon: const Icon(Icons.exit_to_app),
+        ),
+      ),
+      body: const Center(
         child: Text('Home'),
       ),
     );
